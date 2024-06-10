80cb0358064d79e60c4e04d85d4e8cfd4323a885
Brian Kelley <bmkelle@sandia.gov>
2024-06-10 14:59:01 -0600

    Fix unit step pass
    (use MutableValueRange::assign instead of just assigning elements, which
    isn't allowed). Also fix skip condition on IV rewriting

--- mlir/lib/Dialect/Kokkos/Transforms/KokkosPatterns.cpp
+++ mlir/lib/Dialect/Kokkos/Transforms/KokkosPatterns.cpp
@@ -27,6 +27,9 @@ namespace {
 // Is v a compile-time constant integer with value 0?
 bool valueIsIntegerConstantZero(Value v)
 {
+  // If we don't know what op generated v, can't assume anything about its value
+  if (!v.getDefiningOp())
+    return false;
   if (auto constantOp = dyn_cast<arith::ConstantOp>(v.getDefiningOp())) {
     auto valAttr = constantOp.getValue();
     if (auto iAttr = valAttr.dyn_cast<IntegerAttr>()) {
@@ -40,6 +43,9 @@ bool valueIsIntegerConstantZero(Value v)
 // Is v a compile-time constant integer with value 1?
 bool valueIsIntegerConstantOne(Value v)
 {
+  // If we don't know what op generated v, can't assume anything about its value
+  if (!v.getDefiningOp())
+    return false;
   if (auto constantOp = dyn_cast<arith::ConstantOp>(v.getDefiningOp())) {
     auto valAttr = constantOp.getValue();
     if (auto iAttr = valAttr.dyn_cast<IntegerAttr>()) {
@@ -58,7 +64,7 @@ struct ParallelUnitStepRewriter : public OpRewritePattern<scf::ParallelOp> {
 
   LogicalResult matchAndRewrite(scf::ParallelOp op, PatternRewriter &rewriter) const override {
     // n is the dimensionality of loop (number of lower bounds/upper bounds/steps)
-    int n = op.getNumLoops();
+    const int n = op.getNumLoops();
     // If all lower bounds are 0 and all steps are 1, do nothing
     bool allUnitAlready = true;
     for(int i = 0; i < n; i++) {
@@ -81,15 +87,19 @@ struct ParallelUnitStepRewriter : public OpRewritePattern<scf::ParallelOp> {
     //  - Replace upper with (upper - lower + step - 1) / step
     //  - Replace step with 1
     //  - Replace all uses of the old induction variable with "lower + i * step" where i is the new induction variable
+    SmallVector<Value> newLowers;
+    SmallVector<Value> newSteps;
     SmallVector<Value> newUppers;
     for(int i = 0; i < n; i++) {
+      newLowers.push_back(zero);
+      newSteps.push_back(one);
       // If this dimension already has lower 0 and step 1, just keep the old upper
       Value step = op.getStep()[i];
       Value lower = op.getLowerBound()[i];
       if(!valueIsIntegerConstantZero(lower) ||
          !valueIsIntegerConstantOne(step)) {
+        // Note: folding will automatically simplify this arithmetic where possible
         // upper - lower
-        // TODO: does folding automatically eliminate this op if lower is 0?
         Value newUpper = rewriter.create<arith::SubIOp>(op.getLoc(), op.getUpperBound()[i], lower).getResult();
         // upper - lower + step
         newUpper = rewriter.create<arith::AddIOp>(op.getLoc(), newUpper, step).getResult();
@@ -104,22 +114,17 @@ struct ParallelUnitStepRewriter : public OpRewritePattern<scf::ParallelOp> {
         newUppers.push_back(op.getUpperBound()[i]);
       }
     }
-    //TODO: they renamed this to startOpModification in upstream
-    rewriter.startRootUpdate(op);
-    //rewriter.startOpModification(op);
-    auto lowers = op.getLowerBoundMutable();
-    auto uppers = op.getUpperBoundMutable();
-    auto steps = op.getStepMutable();
+    // Now go into the loop body and compute a mapping from new to old induction variable,
+    // and replace all usages of the induction variable with this.
     auto& body = op.getLoopBody();
-    auto inductionVars = op.getInductionVars();
     rewriter.setInsertionPointToStart(&body.front());
     for(int i = 0; i < n; i++) {
-      Value oldLower = lowers[i];
-      Value oldStep = steps[i];
-      Value induction = inductionVars[i];
+      Value oldLower = op.getLowerBound()[i];
+      Value oldStep = op.getStep()[i];
+      Value induction = op.getInductionVars()[i];
       // Skip this dimension is nothing is changing
-      if(!valueIsIntegerConstantZero(oldLower) ||
-         !valueIsIntegerConstantOne(oldStep)) {
+      if(valueIsIntegerConstantZero(oldLower) &&
+         valueIsIntegerConstantOne(oldStep)) {
         continue;
       }
       // Compute lower + i * step (old lower and step, and i is the induction var)
@@ -127,11 +132,13 @@ struct ParallelUnitStepRewriter : public OpRewritePattern<scf::ParallelOp> {
       auto replacementException = rewriter.create<arith::MulIOp>(op.getLoc(), induction, oldStep);
       Value inductionReplacement = rewriter.create<arith::AddIOp>(op.getLoc(), oldLower, replacementException.getResult()).getResult();
       rewriter.replaceAllUsesExcept(induction, inductionReplacement, replacementException);
-      // Finally, update the loop's bounds
-      lowers[i] = zero;
-      uppers[i] = newUppers[i];
-      steps[i] = one;
     }
+    //TODO: they renamed this to startOpModification in upstream
+    rewriter.startRootUpdate(op);
+    //rewriter.startOpModification(op);
+    op.getLowerBoundMutable().assign(newLowers);
+    op.getUpperBoundMutable().assign(newUppers);
+    op.getStepMutable().assign(newSteps);
     //TODO: they renamed this to finalizeOpModification upstream
     rewriter.finalizeRootUpdate(op);
     //rewriter.finalizeOpModification(op);
